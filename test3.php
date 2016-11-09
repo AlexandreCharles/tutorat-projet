8453633540e3f42d024fbf25902f7e968f7cd8a8
AlexandreCharles <charles.alexandre.charles@gmail.com>
2016-11-09 13:43:38 +0100

    Delete test3.php

--- test3.php
+++
@@ -1,7 +0,0 @@
-<p>Date : <input type="text" value="" name="date" id="champ_date" size="12" maxlength="10"></p>
-<div id="calendarMain"></div>
-<script type="text/javascript">
-//<![CDATA[
-calInit("calendarMain", "", "champ_date", "jsCalendar", "day", "selectedDay");
-//]]>
-</script>
